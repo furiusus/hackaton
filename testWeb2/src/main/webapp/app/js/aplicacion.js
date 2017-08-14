7b069fab5294eaea8f34d078858d35980cf0cfbb
furiusus <ronad_1997@hotmail.com>
2017-08-13 22:40:24 -0500

    1.3.0

---
+++ testWeb2/src/main/webapp/app/js/aplicacion.js
@@ -0,0 +1,4 @@
+/**
+ * Created by furiusus on 8/13/17.
+ */
+var app = angular.module('aplicacion',[]);
---
+++ testWeb2/src/main/webapp/app/js/controller/controlador.js
@@ -0,0 +1,13 @@
+/**
+ * Created by furiusus on 8/13/17.
+ */
+
+$( document ).ready(function () {
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
+    $(".collapsible").collapsible();
+});
+
+var controlador=app.controller('MyControler',function ($scope,$http) {
+
+});
