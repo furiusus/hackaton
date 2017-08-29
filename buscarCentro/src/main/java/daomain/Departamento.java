b61e50863fb1ea265dc8391160088fc38e3fa4a8
furiusus <ronad_1997@hotmail.com>
2017-08-29 10:32:00 -0500

    1.4.0

---
+++ buscarCentro/src/main/java/daomain/Departamento.java
@@ -0,0 +1,25 @@
+package daomain;
+
+/**
+ * Created by furiusus on 8/26/17.
+ */
+public class Departamento {
+    private String co_departamento;
+    private String no_departamente;
+
+    public String getCo_departamento() {
+        return co_departamento;
+    }
+
+    public void setCo_departamento(String co_departamento) {
+        this.co_departamento = co_departamento;
+    }
+
+    public String getNo_departamente() {
+        return no_departamente;
+    }
+
+    public void setNo_departamente(String no_departamente) {
+        this.no_departamente = no_departamente;
+    }
+}
